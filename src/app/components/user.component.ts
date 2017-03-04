39a9a75aa0b8a4a656557a8974d1348bfe44cc10
Chase Olsen <chase.olsen22@gmail.com>
2017-03-03 21:12:29 -0700

    Initial Commit

---
+++ src/app/components/user.component.ts
@@ -0,0 +1,70 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'user',
+  template: `
+  <h1>
+  {{name}}
+  </h1>
+  <p><strong>Email:</strong> {{email}}</p>
+  <p><strong>Address:</strong> {{address.street}} {{address.city}} {{address.state}}</p>
+  <button (click)="toggleHobbies()">{{showHobbies ? "Hide Hobbies" : "Show Hobbies"}}</button>
+  <div *ngIf="showHobbies">
+  <h3>Hobbies</h3>
+  <ul>
+    <li *ngFor="let hobby of hobbies">
+      {{hobby}}
+    </li>
+  </ul>
+  </div>
+
+  <form>
+    <label>Name: </label>
+    <input type="text" name="name" [(ngModel)]="name"/><br />
+    <label>Email: </label>
+    <input type="text" name="email" [(ngModel)]="email"/><br />
+    <label>Street: </label>
+    <input type="text" name="address.street" [(ngModel)]="address.street"/><br />
+    <label>City: </label>
+    <input type="text" name="address.city" [(ngModel)]="address.city"/><br />
+    <label>State: </label>
+    <input type="text" name="address.state" [(ngModel)]="address.state"/><br />
+
+  </form>
+
+
+  `,
+})
+export class UserComponent  {
+name: string;
+email: string;
+address: address;
+hobbies: string[];
+showHobbies: boolean;
+
+  constructor(){
+    this.name = 'John Doe';
+    this.email = 'john@gmail.com',
+    this.address = {
+      street: '12 Main st',
+      city: 'Boston',
+      state: 'MA'
+    }
+    this.hobbies = ['Music', 'Movies', 'Sports'];
+    this.showHobbies = false;
+  }
+
+  toggleHobbies(){
+    if(this.showHobbies == true){
+      this.showHobbies = false;
+    } else {
+    this.showHobbies = true;
+    }
+  }
+}
+
+interface address {
+  street: string;
+  city: string;
+  state: string;
+}
