1913785d6a30f3ac43a0c76fe0becd07e9067672
Chase Olsen <chase.olsen22@gmail.com>
2017-03-03 21:46:28 -0700

    Created basic app with routing

--- src/app/components/user.component.ts
+++ src/app/components/user.component.ts
@@ -1,39 +1,11 @@
 import { Component } from '@angular/core';
+import {PostsService} from '../services/post.service';
 
 @Component({
+  moduleId: module.id,
   selector: 'user',
-  template: `
-  <h1>
-  {{name}}
-  </h1>
-  <p><strong>Email:</strong> {{email}}</p>
-  <p><strong>Address:</strong> {{address.street}} {{address.city}} {{address.state}}</p>
-  <button (click)="toggleHobbies()">{{showHobbies ? "Hide Hobbies" : "Show Hobbies"}}</button>
-  <div *ngIf="showHobbies">
-  <h3>Hobbies</h3>
-  <ul>
-    <li *ngFor="let hobby of hobbies">
-      {{hobby}}
-    </li>
-  </ul>
-  </div>
-
-  <form>
-    <label>Name: </label>
-    <input type="text" name="name" [(ngModel)]="name"/><br />
-    <label>Email: </label>
-    <input type="text" name="email" [(ngModel)]="email"/><br />
-    <label>Street: </label>
-    <input type="text" name="address.street" [(ngModel)]="address.street"/><br />
-    <label>City: </label>
-    <input type="text" name="address.city" [(ngModel)]="address.city"/><br />
-    <label>State: </label>
-    <input type="text" name="address.state" [(ngModel)]="address.state"/><br />
-
-  </form>
-
-
-  `,
+  templateUrl: 'user.component.html',
+  providers: [PostsService]
 })
 export class UserComponent  {
 name: string;
@@ -41,8 +13,10 @@ email: string;
 address: address;
 hobbies: string[];
 showHobbies: boolean;
+posts: Post[];
 
-  constructor(){
+
+  constructor(private postsService: PostsService){
     this.name = 'John Doe';
     this.email = 'john@gmail.com',
     this.address = {
@@ -52,6 +26,10 @@ showHobbies: boolean;
     }
     this.hobbies = ['Music', 'Movies', 'Sports'];
     this.showHobbies = false;
+
+    this.postsService.getPosts().subscribe(posts => {
+      this.posts = posts;
+    });
   }
 
   toggleHobbies(){
@@ -61,10 +39,24 @@ showHobbies: boolean;
     this.showHobbies = true;
     }
   }
+
+  addHobby(hobby){
+    this.hobbies.push(hobby);
+  }
+
+  deleteHobby(i){
+    this.hobbies.splice(i, 1);
+  }
 }
 
 interface address {
   street: string;
   city: string;
   state: string;
+}
+
+interface Post {
+  id: number;
+  title: string;
+  body: string;
 }
